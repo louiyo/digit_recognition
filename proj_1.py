9d03c765f16b894fb7aafad926eee538c972e108
BenjamLouis <benjamin.louis@epfl.ch>
2021-04-27 09:56:33 +0200

    Premier réseau fonctionnel - Basse Précision

--- proj_1.py
+++ proj_1.py
@@ -23,7 +23,7 @@ class Net(nn.Module):
         self.conv2 = nn.Conv2d(6, 16, kernel_size= 2)
         self.fc1 = nn.Linear(16, 8)
         self.fc2 = nn.Linear(8, 4)
-        self.fc3 = nn.Linear(4, 1)
+        self.fc3 = nn.Linear(4, 2)
 
     def forward(self, x):
 
@@ -36,7 +36,8 @@ class Net(nn.Module):
         x = self.fc3(x)
         return x
 
-    
+
+#Attention il faudra modifier ça y'apaldroit
     def num_flat_features(self, x):
 
         size = x.size()[1:]  # all dimensions except the batch dimension
@@ -47,18 +48,14 @@ class Net(nn.Module):
 
 
 def train_model(model, train_input, train_target, mini_batch_size = 25):
-    criterion = nn.MSELoss()
+    criterion = nn.CrossEntropyLoss()
     optimizer = optim.SGD(model.parameters(), lr = 1e-1)
-    nb_epochs = 250
+    nb_epochs = 100
 
     for e in range(nb_epochs):
         for b in range(0, train_input.size(0), mini_batch_size):
             output = model(train_input.narrow(0, b, mini_batch_size))
-            print(output)
-            temp = output.flatten()
-            print(temp)
-            print(train_target.narrow(0, b, mini_batch_size))
-            loss = criterion(temp, train_target.narrow(0, b, mini_batch_size))
+            loss = criterion(output, train_target.narrow(0, b, mini_batch_size))
             model.zero_grad()
             loss.backward()
             optimizer.step()
@@ -77,6 +74,9 @@ def compute_nb_errors(model, data_input, data_target, mini_batch_size = 25):
         output = model(data_input.narrow(0, b, mini_batch_size))
         _, predicted_classes = torch.max(output, 1)
         for k in range(mini_batch_size):
+            print(data_target[b+k])
+            print(predicted_classes[k])
+            print("|----|")
             if data_target[b + k] != predicted_classes[k]:
                 nb_data_errors = nb_data_errors + 1
 
